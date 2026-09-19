4cc544b8ba3676b62526d5062c7b08b59582a77b
Dmitry Ves <dmitry.ves@gmail.com>
2026-09-19 23:42:10 +1000

    Hide environment separator when unset

--- app/frontend/40-app-env.sh
+++ app/frontend/40-app-env.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
-envsubst '${APP_ENV}' \
+if [ -n "$APP_ENV" ]; then
+  APP_ENV_LABEL=" — $APP_ENV"
+else
+  APP_ENV_LABEL=""
+fi
+
+export APP_ENV_LABEL
+
+envsubst '${APP_ENV_LABEL}' \
   < /usr/share/nginx/html/index.template.html \
   > /usr/share/nginx/html/index.html
